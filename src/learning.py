803255eea2e0443835a79cd9854d827118e15310
fraa_cicala <francycicala04@gmail.com>
2025-12-19 18:49:16 +0100

    Definita funzione per il decision tree.

--- src/learning.py
+++ src/learning.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 import sklearn as sk
+from pandas import crosstab
 from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, cross_validate;
+import os
 
 #Funzione per caricare il dataset specificato utilizzando il suo percorso.
 #Utilizza la libreria pandas per leggere il file csv.
@@ -21,3 +25,43 @@ def preprocessing_dataset(df):
     X = df[columns_features]
     y=df['Verdict']
     return X, y
+
+def decisiontree_classifier(X,y):
+    skf = StratifiedKFold(n_splits=5, shuffle=True)
+    model = DecisionTreeClassifier()
+    parametri = {
+        'criterion': ['gini', 'entropy'],
+        'max_depth': [None, 3, 5, 10],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 5]
+    }
+    grid_search = GridSearchCV(model, parametri, cv=skf)
+    grid_search.fit(X, y)
+    best_model = grid_search.best_estimator_
+    print(f"Parametri migliori:  {grid_search.best_params_}")
+    metriche = ['accuracy_weighted','precision_weighted','recall_weighted','f1_weighted']
+    risultati = cross_validate(best_model, X, y, cv=skf, scoring=metriche)
+    print(f"Accuracy Media:  {risultati['test_accuracy'].mean():.2%} (+/- {risultati['test_accuracy'].std():.2%})")
+    print(f"Precision Media: {risultati['test_precision_weighted'].mean():.2%}")
+    print(f"Recall Media:    {risultati['test_recall_weighted'].mean():.2%}")
+    print(f"F1-Score Media:  {risultati['test_f1_weighted'].mean():.2%}")
+    return best_model
+
+
+
+
+
+
+
+
+
+
+
+
+parametri = {
+    'n_estimators': [50, 100],
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [None, 5, 10],
+    'min_samples_split': [2, 5],
+    'min_samples_leaf': [1, 2]
+}
